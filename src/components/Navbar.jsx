e6d598ac334cc90f39eb598f3a2273e0705dabb6
wissalcodes <lw_messikh@esi.dz>
2023-09-11 01:23:05 +0100

    ReactTyped, responsive font size and React icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,13 +15,13 @@ export default function Navbar(){
                 <li className="p-4">About</li>
                 <li className="p-4">Contact</li>
             </ul>
-            <div onClick={handleNav} className="block md:hidden pt-8">
+            <div onClick={handleNav} className="block md:hidden pt-8 pr-12">
                 {
                     !nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}></AiOutlineMenu>
                 }
             </div>
             <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900  m-4 ease-in-out duration-500" : "fixed top-0 w-[60%] h-full border-r border-r-gray-900  m-4 ease-in-out duration-500 left-[-100%]" }>
-                <ul className="p-4 pt-12 uppercase block md:hidden">
+                <ul className="pt-12 uppercase block md:hidden">
                     <li className="p-4 border-b border-gray-600">Home</li>
                     <li className="p-4 border-b border-gray-600">Company</li>
                     <li className="p-4 border-b border-gray-600">Resources</li>
