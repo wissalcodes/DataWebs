3e249a5761a709d77fde0b43aabf7ec6ba23240b
wissalcodes <lw_messikh@esi.dz>
2023-09-12 19:10:38 +0100

    newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
 
   )
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,8 +2,11 @@ import React from "react";
 export default function Newsletter(){
     return(
         <div className="w-full py-16 text-white">
-            <div className="max-w-[1240px] mx-auto grid lg:">
-
+            <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
+                <div>
+                    <h1 className="font-bold md:text-4xl sm:text-3xl text-x2l">Want tips and tricks to optimize your flow?</h1>
+                    <p>Sign up to our Newsletter and stay up to date.</p>
+                </div>
             </div>
         </div>
     )
